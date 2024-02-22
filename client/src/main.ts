fa71110c4d799f163dac71cd0d7eb649475fe5bd
Eric BARNET <70975601+EricBarnet@users.noreply.github.com>
2024-02-22 14:11:18 +0100

    Ajout de Angular Material

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,9 +3,10 @@ import { AppComponent } from './app/app.component';
 import { RouterModule } from '@angular/router';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(RouterModule.forRoot(routes))
+    importProvidersFrom(RouterModule.forRoot(routes)), provideAnimationsAsync()
   ]
 });
