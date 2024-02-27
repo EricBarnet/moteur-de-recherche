44ddbc25a042c073c63fe9a76b0d4e94872d05f3
Eric BARNET <70975601+EricBarnet@users.noreply.github.com>
2024-02-27 10:07:15 +0100

    Configuration service Book

--- client/src/app/services/book.service.ts
+++ client/src/app/services/book.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Book } from '../models/book.model';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
+  private mockBooks: Book[] = [
+    {
+      id: 1,
+      title: 'Livre Un',
+      author: 'Auteur Un',
+      description: 'Description du Livre Un',
+    },
+    {
+      id: 2,
+      title: 'Livre Deux',
+      author: 'Auteur Deux',
+      description: 'Description du Livre Deux',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  // Récupérer les livres
+  getBooks(): Observable<Book[]> {
+    return of(this.mockBooks);
+  }
 }
