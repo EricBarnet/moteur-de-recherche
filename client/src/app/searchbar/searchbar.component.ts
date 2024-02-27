aa9635fd65bba2f21210526823fb596b2ee77ae8
Eric BARNET <70975601+EricBarnet@users.noreply.github.com>
2024-02-27 10:09:33 +0100

    Mock data in searchbar

--- client/src/app/searchbar/searchbar.component.ts
+++ client/src/app/searchbar/searchbar.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Book } from '../models/book.model';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-searchbar',
@@ -18,6 +20,21 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.scss',
 })
-export class SearchbarComponent {
+export class SearchbarComponent implements OnInit {
+  books: Book[] = [];
+
+  constructor(private bookService: BookService) {
+  }
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  getBooks(): void {
+    this.bookService.getBooks().subscribe((books) => {
+      this.books = books;
+    });
+  }
+  
   keyword: string = '';
 }
