6fa4a8cbaaee8ead7b127890bfc51f6957c6b300
Eric BARNET <70975601+EricBarnet@users.noreply.github.com>
2024-02-27 09:34:31 +0100

    Création composant list et ajout dans home

--- client/src/app/list/list.component.ts
+++ client/src/app/list/list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
